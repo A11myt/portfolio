10841ab558b7b63d3fe3c76c1439604eb6743c96
Jason <Jason.j@gmx.de>
2024-12-17 14:48:57 +0100

    changed Head source

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
+import { Metadata } from "next";
+import Head from "next/head";
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Jason Johnson - Portfolio</title>
+      <title>Jason Johnson - Portfolio</title>
         <meta name="description" content="Jason Johnson's portfolio website" />
-        <meta name="keywords" content="Jason Johnson, Portfolio, Web Developer" />
-      </Head>
+        <meta name="keywords" content="Jason Johnson, Portfolio, Web Developer" />        
+        </Head>
       <body className="antialiased">
-        <Main />
+        <Main  />
         <NextScript />
       </body>
     </Html>

49125bcea0efa6e22027865cffb2392a1f69c2fd
A11myt <Jason.j@gmx.de>
2025-12-12 05:40:51 +0000

    ---

--- pages/manifest/index.tsx
+++ pages/manifest/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 // import { useRouter } from 'next/router';
 // import { Language } from '@/src/types/Types';
 import { Metadata } from 'next';
